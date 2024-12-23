6148f5814c1c14f00ec9c790aea1e37f06ee7044
Rachatapon Dev <116191109+RachataponDev@users.noreply.github.com>
2024-12-24 00:16:59 +0700

    newbie

---
+++ app/components/nav.js
@@ -0,0 +1,36 @@
+"use client";
+
+export default function NavBar() {
+  return (
+    <nav className="nav">
+      <div className="center">
+        <div className="logo">
+          <img src="/index/btc.jpg" alt="Logo" />
+          <span>Hello Blockchain</span>
+        </div>
+        <ul>
+          <li className="active">
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Feature</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Sign In</a>
+          </li>
+        </ul>
+        <div className="btn_inp">
+          <button className="btn_up">
+            <a>Sign Up</a>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,13 +1,27 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-
-import ScoreBoard from "./components/scoreboard"
+import Navbar from "./components/nav"; // Renamed to PascalCase for React component
 
 export default function Home() {
   return (
-    <div>
-<h1>Hello</h1>
-      <ScoreBoard/>
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles.bg}></div>
+      <div className={styles.main}>
+        <div className={styles.txt_contain}>
+        <div className={styles.left}>
+        <p>Some text Here lol</p>
+        <span>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries
+        </span>
+        <div className={styles.line}></div>
+        <input type="text" className={styles.inpser} placeholder="Serch Here"/>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
